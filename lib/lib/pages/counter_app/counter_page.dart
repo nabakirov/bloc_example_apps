232487948221c2df7fb8d9d017ae4f75d0f4925c
Nurs <nabakirov@gmail.com>
2019-08-09 15:56:13 +0600

    add counter app

---
+++ lib/lib/pages/counter_app/counter_page.dart
@@ -0,0 +1,45 @@
+import 'package:bloc_counter/pages/counter_app/counter_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/material.dart';
+
+class CounterPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<CounterBloc>(
+        builder: (context) => CounterBloc(), child: CounterWidget());
+  }
+}
+
+class CounterWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final CounterBloc counterBloc = BlocProvider.of<CounterBloc>(context);
+    return Scaffold(
+        appBar: AppBar(title: Text('bloc counter')),
+        body: BlocBuilder<CounterBloc, int>(builder: (context, count) {
+          return Center(child: Text('$count', style: TextStyle(fontSize: 24)));
+        }),
+        floatingActionButton: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Padding(
+                padding: EdgeInsets.symmetric(vertical: 5),
+                child: FloatingActionButton(
+                  child: Icon(Icons.add),
+                  onPressed: () {
+                    counterBloc.dispatch(CounterEvent.increment);
+                  },
+                )),
+            Padding(
+                padding: EdgeInsets.symmetric(vertical: 5),
+                child: FloatingActionButton(
+                  child: Icon(Icons.remove),
+                  onPressed: () {
+                    counterBloc.dispatch(CounterEvent.decrement);
+                  },
+                ))
+          ],
+        ));
+  }
+}
